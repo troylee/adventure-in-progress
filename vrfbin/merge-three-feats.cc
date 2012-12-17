102742d1e511b9afd549ba569013c79bb52f7168
troy.lee2008@gmail.com <troy.lee2008@gmail.com@d2dc0ceb-8a10-0df8-ca54-0c7c990c9a2b>
2012-12-17 03:13:58 +0000

    feature combination

---
+++ vrfbin/merge-three-feats.cc
@@ -0,0 +1,95 @@
+/*
+ * vrfbin/merge-three-feats.cc
+ *
+ *  Created on: Dec 17, 2012
+ *      Author: Troy Lee (troy.lee2008@gmail.com)
+ *
+ * Merge three features frame by frame.
+ *
+ */
+
+#include "base/kaldi-common.h"
+#include "util/common-utils.h"
+#include "matrix/kaldi-matrix.h"
+
+int main(int argc, char *argv[]) {
+  try {
+    using namespace kaldi;
+    typedef kaldi::int32 int32;
+
+    const char *usage =
+        "Merge three features frame by frame.\n"
+            "Usage: merge-three-feats [options] feats1-rspecifier feats2-rspecifier "
+            "feats3-rspecifier out-feats-wspecifier\n";
+
+    ParseOptions po(usage);
+
+    po.Read(argc, argv);
+
+    if (po.NumArgs() != 4) {
+      po.PrintUsage();
+      exit(1);
+    }
+
+    std::string feats1_rspecifier = po.GetArg(1);
+    std::string feats2_rspecifier = po.GetArg(2);
+    std::string feats3_rspecifier = po.GetArg(3);
+    std::string out_feats_wspecifier = po.GetArg(4);
+
+    SequentialBaseFloatMatrixReader feats1_reader(feats1_rspecifier);
+    RandomAccessBaseFloatMatrixReader feats2_reader(feats2_rspecifier);
+    RandomAccessBaseFloatMatrixReader feats3_reader(feats3_rspecifier);
+    BaseFloatMatrixWriter out_feats_writer(out_feats_wspecifier);
+
+    int32 num_done = 0;
+
+    for (; !feats1_reader.Done(); feats1_reader.Next()) {
+      std::string key = feats1_reader.Key();
+      Matrix<BaseFloat> feats1(feats1_reader.Value());
+
+      if (!feats2_reader.HasKey(key)) {
+        KALDI_WARN<< "No matching features available in the 2nd features for key "
+        << key << ", producing no output for this utterance";
+        continue;
+      }
+
+      if (!feats3_reader.HasKey(key)) {
+        KALDI_WARN<< "No matching features available in the 3rd features for key "
+        << key << ", producing no output for this utterance";
+        continue;
+      }
+
+      Matrix<BaseFloat> feats2(feats2_reader.Value(key));
+      Matrix<BaseFloat> feats3(feats3_reader.Value(key));
+
+      KALDI_ASSERT(feats1.NumRows()==feats2.NumRows() && feats1.NumRows()==feats3.NumRows());
+
+      Matrix<BaseFloat> out_feats(feats1.NumRows(),
+                                  feats1.NumCols() + feats2.NumCols() + feats3.NumCols());
+
+      SubMatrix<BaseFloat> left(out_feats, 0, feats1.NumRows(), 0,
+                                feats1.NumCols());
+      left.CopyFromMat(feats1, kNoTrans);
+
+      SubMatrix<BaseFloat> mid(out_feats, 0, feats1.NumRows(), feats1.NumCols(), feats2.NumCols());
+      mid.CopyFromMat(feats2, kNoTrans);
+
+      SubMatrix<BaseFloat> right(out_feats, 0, feats1.NumRows(),
+                                 feats1.NumCols()+feats2.NumCols(), feats3.NumCols());
+      right.CopyFromMat(feats3, kNoTrans);
+
+      out_feats_writer.Write(key, out_feats);
+
+      ++num_done;
+      if (num_done % 1000 == 0) {
+        KALDI_LOG<< "Done " << num_done << " utterances.";
+      }
+    }
+
+    return 0;
+  } catch (const std::exception &e) {
+    std::cerr << e.what();
+    return -1;
+  }
+}
+
---
+++ vrfbin/merge-two-feats.cc
@@ -0,0 +1,78 @@
+/*
+ * vrfbin/merge-two-feats.cc
+ *
+ *  Created on: Dec 17, 2012
+ *      Author: Troy Lee (troy.lee2008@gmail.com)
+ *
+ * Merge two features frame by frame.
+ *
+ */
+
+#include "base/kaldi-common.h"
+#include "util/common-utils.h"
+#include "matrix/kaldi-matrix.h"
+
+int main(int argc, char *argv[]) {
+  try {
+    using namespace kaldi;
+    typedef kaldi::int32 int32;
+
+    const char *usage =
+        "Merge two features frame by frame.\n"
+            "Usage: merge-two-feats [options] feats1-rspecifier feats2-rspecifier "
+            "out-feats-wspecifier\n";
+
+    ParseOptions po(usage);
+
+    po.Read(argc, argv);
+
+    if (po.NumArgs() != 3) {
+      po.PrintUsage();
+      exit(1);
+    }
+
+    std::string feats1_rspecifier = po.GetArg(1);
+    std::string feats2_rspecifier = po.GetArg(2);
+    std::string out_feats_wspecifier = po.GetArg(3);
+
+    SequentialBaseFloatMatrixReader feats1_reader(feats1_rspecifier);
+    RandomAccessBaseFloatMatrixReader feats2_reader(feats2_rspecifier);
+    BaseFloatMatrixWriter out_feats_writer(out_feats_wspecifier);
+
+    int32 num_done = 0;
+
+    for (; !feats1_reader.Done(); feats1_reader.Next()) {
+      std::string key = feats1_reader.Key();
+      Matrix<BaseFloat> feats1(feats1_reader.Value());
+
+      if (!feats2_reader.HasKey(key)) {
+        KALDI_WARN<< "No matching features available for key "
+        << key << ", producing no output for this utterance";
+        continue;
+      }
+
+      Matrix<BaseFloat> feats2(feats2_reader.Value(key));
+
+      KALDI_ASSERT(feats1.NumRows()==feats2.NumRows());
+
+      Matrix<BaseFloat> out_feats(feats1.NumRows(), feats1.NumCols()+feats2.NumCols());
+      SubMatrix<BaseFloat> left(out_feats, 0, feats1.NumRows(), 0, feats1.NumCols());
+      left.CopyFromMat(feats1, kNoTrans);
+      SubMatrix<BaseFloat> right(out_feats, 0, feats1.NumRows(), feats1.NumCols(), feats2.NumCols());
+      right.CopyFromMat(feats2, kNoTrans);
+
+      out_feats_writer.Write(key, out_feats);
+
+      ++num_done;
+      if (num_done % 1000 == 0) {
+        KALDI_LOG<< "Done " << num_done << " utterances.";
+      }
+    }
+
+    return 0;
+  } catch (const std::exception &e) {
+    std::cerr << e.what();
+    return -1;
+  }
+}
+
