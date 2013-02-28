54efd0f56d601799257b3686325d3426c6c1a45a
troy.lee2008@gmail.com <troy.lee2008@gmail.com@d2dc0ceb-8a10-0df8-ca54-0c7c990c9a2b>
2013-02-28 03:08:15 +0000

    Add VTS for UBM

--- trunk/vtsbin/gmm-global-get-frame-comp-likes.cc
+++ trunk/vtsbin/gmm-global-get-frame-comp-likes.cc
@@ -7,6 +7,8 @@
 #include "gmm/diag-gmm.h"
 #include "gmm/mle-full-gmm.h"
 
+#include "vts/vts-first-order.h"
+
 int main(int argc, char *argv[]) {
   try {
     using namespace kaldi;
@@ -15,29 +17,41 @@ int main(int argc, char *argv[]) {
         "Print out per-frame pre-Gaussian log-likelihoods for each utterance, as an archive\n"
             "of matrices of floats.\n"
             "Usage:  gmm-global-get-frame-comp-likes [options] <model-in> <feature-rspecifier> "
-            "<likes-out-wspecifier>\n"
+            "<likes-out-wspecifier> [<noise-rspecifier>]\n"
             "e.g.: gmm-global-get-frame-comp-likes 1.mdl scp:train.scp ark:1.likes\n";
 
     ParseOptions po(usage);
-    std::string gselect_rspecifier;
-    po.Register("gselect", &gselect_rspecifier,
-                "rspecifier for gselect objects "
-                "to limit the #Gaussians accessed on each frame.");
 
     bool apply_log = true;
     po.Register("apply-log", &apply_log,
                 "Output log likelihoods or likelihoods.");
 
+    int32 num_cepstral = 13;
+    int32 num_fbank = 26;
+    BaseFloat ceplifter = 22;
+
+    po.Register("num-cepstral", &num_cepstral, "Number of Cepstral features");
+    po.Register("num-fbank", &num_fbank,
+                "Number of FBanks used to generate the Cepstral features");
+    po.Register("ceplifter", &ceplifter,
+                "CepLifter value used for feature extraction");
+
     po.Read(argc, argv);
 
-    if (po.NumArgs() != 3) {
+    if (po.NumArgs() != 3 && po.NumArgs() != 4) {
       po.PrintUsage();
       exit(1);
     }
 
+    bool have_noise = false;
+    if (po.NumArgs() == 4) {
+      have_noise = true;
+    }
+
     std::string model_filename = po.GetArg(1),
         feature_rspecifier = po.GetArg(2),
-        likes_wspecifier = po.GetArg(3);
+        likes_wspecifier = po.GetArg(3),
+        noise_rspecifier = po.GetOptArg(4);
 
     DiagGmm gmm;
     {
@@ -46,10 +60,14 @@ int main(int argc, char *argv[]) {
       gmm.Read(ki.Stream(), binary_read);
     }
 
+    Matrix<double> dct_mat, inv_dct_mat;
+    GenerateDCTmatrix(num_cepstral, num_fbank, ceplifter, &dct_mat,
+                      &inv_dct_mat);
+
     double tot_like = 0.0, tot_frames = 0.0;
 
     SequentialBaseFloatMatrixReader feature_reader(feature_rspecifier);
-    RandomAccessInt32VectorVectorReader gselect_reader(gselect_rspecifier);
+    RandomAccessDoubleVectorReader noiseparams_reader(noise_rspecifier);
     BaseFloatMatrixWriter likes_writer(likes_wspecifier);
     int32 num_done = 0, num_err = 0;
 
@@ -59,44 +77,52 @@ int main(int argc, char *argv[]) {
       int32 file_frames = mat.NumRows();
       Matrix<BaseFloat> likes(file_frames, gmm.NumGauss());
 
-      if (gselect_rspecifier != "") {
-        if (!gselect_reader.HasKey(key)) {
-          KALDI_WARN<< "No gselect information for utterance " << key;
-          num_err++;
-          continue;
-        }
-        const std::vector<std::vector<int32> > &gselect =
-        gselect_reader.Value(key);
-        if (gselect.size() != static_cast<size_t>(file_frames)) {
-          KALDI_WARN << "gselect information for utterance " << key
-          << " has wrong size " << gselect.size() << " vs. "
-          << file_frames;
-          num_err++;
-          continue;
-        }
-
-        for (int32 i = 0; i < file_frames; i++) {
-          SubVector<BaseFloat> data(mat, i);
-          const std::vector<int32> &this_gselect = gselect[i];
-          int32 gselect_size = this_gselect.size();
-          KALDI_ASSERT(gselect_size > 0);
-          Vector<BaseFloat> loglikes;
-          gmm.LogLikelihoodsPreselect(data, this_gselect, &loglikes);
-          if(!apply_log) {
-            loglikes.ApplyExp();
-          }
-          likes.CopyRowFromVec(loglikes, i);
-        }
-      } else {  // no gselect..
+      if (!have_noise) {
+        // no noise..
         for (int32 i = 0; i < file_frames; i++) {
           Vector<BaseFloat> loglikes;
           gmm.LogLikelihoods(mat.Row(i), &loglikes);
-          if(!apply_log) {
+          if (!apply_log) {
             loglikes.ApplyExp();
           }
           likes.CopyRowFromVec(loglikes, i);
         }
+      } else {
+        // have noise
 
+        if (!noiseparams_reader.HasKey(key + "_mu_h")
+            || !noiseparams_reader.HasKey(key + "_mu_z")
+            || !noiseparams_reader.HasKey(key + "_var_z")) {
+          KALDI_ERR<< "Not all the noise parameters (mu_h, mu_z, var_z) are available!";
+        }
+
+        int feat_dim = mat.NumCols();
+        if (feat_dim != 39) {
+          KALDI_ERR << "Could not decode the features, only 39D MFCC_0_D_A is supported!";
+        }
+
+        /************************************************
+         Extract the noise parameters
+         *************************************************/
+
+        Vector<double> mu_h(noiseparams_reader.Value(key + "_mu_h"));
+        Vector<double> mu_z(noiseparams_reader.Value(key + "_mu_z"));
+        Vector<double> var_z(noiseparams_reader.Value(key + "_var_z"));
+
+        DiagGmm noise_gmm;
+        noise_gmm.CopyFromDiagGmm(gmm);
+
+        std::vector<Matrix<double> > Jx(gmm.NumGauss()), Jz(gmm.NumGauss());  // not necessary for compensation only
+        CompensateDiagGmm(mu_h, mu_z, var_z, num_cepstral, num_fbank, dct_mat, inv_dct_mat, noise_gmm, Jx, Jz);
+
+        for (int32 i = 0; i < file_frames; i++) {
+          Vector<BaseFloat> loglikes;
+          noise_gmm.LogLikelihoods(mat.Row(i), &loglikes);
+          if (!apply_log) {
+            loglikes.ApplyExp();
+          }
+          likes.CopyRowFromVec(loglikes, i);
+        }
       }
 
       tot_like += likes.Sum();
