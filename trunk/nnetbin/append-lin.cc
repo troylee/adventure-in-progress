81ed13f0d48e50e821b79a81f6cf859f711dbe56
troy.lee2008@gmail.com <troy.lee2008@gmail.com@d2dc0ceb-8a10-0df8-ca54-0c7c990c9a2b>
2013-03-13 05:33:57 +0000

    bug fix

--- trunk/nnetbin/append-lin.cc
+++ trunk/nnetbin/append-lin.cc
@@ -18,9 +18,9 @@ int main(int argc, char *argv[]) {
 
     const char *usage =
         "Append a LIN to the existing nnet.\n"
-        "Usage:  append-lin [options] <nnet-in> <nnet-out>\n"
-        "e.g.:\n"
-        " append-lin --binary=false nnet.mdl nnet_lin.mdl\n";
+            "Usage:  append-lin [options] <nnet-in> <nnet-out>\n"
+            "e.g.:\n"
+            " append-lin --binary=false nnet.mdl nnet_lin.mdl\n";
 
     ParseOptions po(usage);
 
@@ -31,9 +31,9 @@ int main(int argc, char *argv[]) {
     int32 blockdim = 123;
     int32 numblocks = 9;
     po.Register("diagonal-block", &diagblock, "Use diagonal block LIN");
-    po.Register("block-dim", &blockdim, "Dimension of each block (square block)");
+    po.Register("block-dim", &blockdim,
+                "Dimension of each block (square block)");
     po.Register("num-blocks", &numblocks, "Number of blocks in the LIN");
-    
 
     po.Read(argc, argv);
 
@@ -45,52 +45,50 @@ int main(int argc, char *argv[]) {
     std::string model_in_filename = po.GetArg(1),
         model_out_filename = po.GetArg(2);
 
-    Nnet nnet; 
+    Nnet nnet;
     {
       bool binary_read;
       Input ki(model_in_filename, &binary_read);
       nnet.Read(ki.Stream(), binary_read);
     }
-    
-    Component *lin;
+
     int32 in_dim = nnet.InputDim();
-
-    if(diagblock){
-      if(blockdim * numblocks != in_dim){
-        KALDI_ERR<< "Invalid block configuration: [dim: " << blockdim << ", num: " << numblocks << "], input dim: " << in_dim;
-      }
-      MaskedBL mbl(in_dim, in_dim, NULL);
-      mbl.SetToIdentity();
-      Matrix<BaseFloat> mask(in_dim, in_dim, kSetZero);
-      for(int32 i=0, offset=0; i<numblocks; ++i, offset+=blockdim){
-        for(int32 j=0; j<blockdim; ++j){
-          for(int32 k=0; k<blockdim; ++k){
-            mask(offset+j, offset+k)=1.0;
-          }
-        }
-      }
-      mbl.SetMask(mask);
-
-      lin=&mbl;
-
-    } else {
-      BiasedLinearity bl(in_dim, in_dim, NULL);
-      bl.SetToIdentity();
-
-      lin=&bl;
-    }
 
     {
       Output ko(model_out_filename, binary_write);
-      lin->Write(ko.Stream(), binary_write);
+
+      if (diagblock) {
+        if (blockdim * numblocks != in_dim) {
+          KALDI_ERR<< "Invalid block configuration: [dim: " << blockdim << ", num: " << numblocks << "], input dim: " << in_dim;
+        }
+        MaskedBL mbl(in_dim, in_dim, NULL);
+        mbl.SetToIdentity();
+        Matrix<BaseFloat> mask(in_dim, in_dim, kSetZero);
+        for(int32 i=0, offset=0; i<numblocks; ++i, offset+=blockdim) {
+          for(int32 j=0; j<blockdim; ++j) {
+            for(int32 k=0; k<blockdim; ++k) {
+              mask(offset+j, offset+k)=1.0;
+            }
+          }
+        }
+        mbl.SetMask(mask);
+
+        mbl.Write(ko.Stream(), binary_write);
+
+      } else {
+        BiasedLinearity bl(in_dim, in_dim, NULL);
+        bl.SetToIdentity();
+
+        bl.Write(ko.Stream(), binary_write);
+      }
+
       nnet.Write(ko.Stream(), binary_write);
     }
 
-    KALDI_LOG << "Written model to " << model_out_filename;
+    KALDI_LOG<< "Written model to " << model_out_filename;
   } catch(const std::exception& e) {
     std::cerr << e.what() << '\n';
     return -1;
   }
 }
-
 
