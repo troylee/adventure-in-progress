5d95c9f3ea9b18121614756b5f796ffe9585237c
troy.lee2008@gmail.com <troy.lee2008@gmail.com@d2dc0ceb-8a10-0df8-ca54-0c7c990c9a2b>
2013-05-15 07:23:40 +0000

    nnetbin: still in progress

--- trunk/nnetbin/grbm-train-frmshuff.cc
+++ trunk/nnetbin/grbm-train-frmshuff.cc
@@ -1,19 +1,8 @@
-// rbmbin/rbm-train-cd1-frmshuff.cc
-
-// Copyright 2012  Karel Vesely
-
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//  http://www.apache.org/licenses/LICENSE-2.0
-//
-// THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
-// WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
-// MERCHANTABLITY OR NON-INFRINGEMENT.
-// See the Apache 2 License for the specific language governing permissions and
-// limitations under the License.
+// nnetbin/grbm-train-frmshuff.cc
+/*
+ * Troy Lee
+ *
+ */
 
 #include "nnet/nnet-grbm.h"
 #include "nnet/nnet-nnet.h"
@@ -59,6 +48,7 @@ int main(int argc, char *argv[]) {
   typedef kaldi::int32 int32;
 
   try {
+
     const char *usage =
         "Perform GRBM training by contrastive divergence alg.\n"
             "Usage:  grbm-train-frmshuff [options] <model-in> <feature-rspecifier> <model-out> [<epoch-weight>]\n"
