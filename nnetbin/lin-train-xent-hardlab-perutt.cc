2e4974ff013631f1498cb3fab766d07bffaf580f
troy.lee2008@gmail.com <troy.lee2008@gmail.com@d2dc0ceb-8a10-0df8-ca54-0c7c990c9a2b>
2013-07-29 13:03:37 +0000

    utterance based LIN estimation

--- nnetbin/lin-train-xent-hardlab-perutt.cc
+++ nnetbin/lin-train-xent-hardlab-perutt.cc
@@ -1,4 +1,8 @@
 // nnetbin/lin-train-xent-hardlab-perutt.cc
+/*
+ * Train a LIN per utterance and save the transforms in archive file.
+ * Due to the only utterance, train and cv are the same.
+ */
 
 #include "nnet/nnet-nnet.h"
 #include "nnet/nnet-loss.h"
@@ -12,22 +16,34 @@ int main(int argc, char *argv[]) {
   using namespace kaldi;
   try {
     const char *usage =
-        "Train LinBL for each utterance.\n"
-            "Usage: lin-train-xent-hardlab-perutt [options] <model-in> <feature-rspecifier> <alignments-rspecifier> [<model-out-wspecifier>]\n"
+        "Train LIN for each utterance.\n"
+            "Usage: lin-train-xent-hardlab-perutt [options] <model-in> <feature-rspecifier> <alignments-rspecifier>"
+            "<weights-wspecifier> <bias-wspecifier>\n"
             "e.g.: \n"
-            " lin-train-xent-hardlab-perutt nnet.init scp:train.scp ark:train.ali ark:lin.iter1\n";
+            " lin-train-xent-hardlab-perutt lin.init scp:train.scp ark:train.ali ark:weights.ark ark:bias.ark\n";
 
     ParseOptions po(usage);
-    bool crossvalidate = false;
-    po.Register("cross-validate", &crossvalidate,
-                "Perform cross-validation (don't backpropagate)");
 
-    BaseFloat learn_rate = 0.008,
+    int32 max_iters = 10;
+    po.Register("max-iters", &max_iters,
+                "Maixmum number of training iterations");
+
+    BaseFloat learn_rate_init_val = 0.001,
+        learn_rate_end_val = 0.00001,
+        start_halving_inc = 0.5,
+        halving_factor = 0.5,
         momentum = 0.0,
         l2_penalty = 0.0,
         l1_penalty = 0.0;
 
-    po.Register("learn-rate", &learn_rate, "Learning rate");
+    po.Register("learn-rate-init-val", &learn_rate_init_val,
+                "Initial learning rate");
+    po.Register("learn-rate-end-val", &learn_rate_end_val,
+                "Stopping criterion");
+    po.Register("start-havling-inc", &start_halving_inc,
+                "Learning rate halving starting condition");
+    po.Register("halving-factor", &halving_factor,
+                "Learning rate halving factor");
     po.Register("momentum", &momentum, "Momentum");
     po.Register("l2-penalty", &l2_penalty, "L2 penalty (weight decay)");
     po.Register("l1-penalty", &l1_penalty, "L1 penalty (promote sparsity)");
@@ -36,27 +52,18 @@ int main(int argc, char *argv[]) {
     po.Register("feature-transform", &feature_transform,
                 "Feature transform Neural Network");
 
-    std::string init_weight = "", init_bias = "";
-    po.Register("init-weight", &init_weight,
-                "Initial weight matrices for LIN xform");
-    po.Register("init-bias", &init_bias, "Initial biases for LIN xform");
-
     po.Read(argc, argv);
 
-    if (po.NumArgs() != 5 - (crossvalidate ? 2 : 0)) {
+    if (po.NumArgs() != 5) {
       po.PrintUsage();
       exit(1);
     }
 
-    std::string model_filename = po.GetArg(1),
+    std::string si_model_filename = po.GetArg(1),
         feature_rspecifier = po.GetArg(2),
-        alignments_rspecifier = po.GetArg(3);
-
-    std::string target_weight_wspecifier, target_bias_wspecifier;
-    if (!crossvalidate) {
-      target_weight_wspecifier = po.GetArg(4);
-      target_bias_wspecifier = po.GetArg(5);
-    }
+        alignments_rspecifier = po.GetArg(3),
+        weight_wspecifier = po.GetArg(4),
+        bias_wspecifier = po.GetArg(5);
 
     using namespace kaldi;
     typedef kaldi::int32 int32;
@@ -67,16 +74,16 @@ int main(int argc, char *argv[]) {
     }
 
     Nnet nnet;
-    nnet.Read(model_filename);
+    nnet.Read(si_model_filename);
 
-    // construct the learn factors, only learn the first LIN layer
+    // construct the learn factors, only learn the first LIN layer,
+    // i.e. "1,0,0,0,...,0"
     std::string learn_factors = "1";
     for (int32 i = 1; i < nnet.LayerCount(); ++i) {
       if (nnet.Layer(i)->IsUpdatable())
         learn_factors += ",0";
     }
 
-    nnet.SetLearnRate(learn_rate, learn_factors.c_str());
     nnet.SetMomentum(momentum);
     nnet.SetL2Penalty(l2_penalty);
     nnet.SetL1Penalty(l1_penalty);
@@ -86,37 +93,41 @@ int main(int argc, char *argv[]) {
     }
     LinBL *lin = static_cast<LinBL*>(nnet.Layer(0));
 
-    kaldi::int64 tot_t = 0;
-
     SequentialBaseFloatMatrixReader feature_reader(feature_rspecifier);
     RandomAccessInt32VectorReader alignments_reader(alignments_rspecifier);
 
-    RandomAccessBaseFloatMatrixReader init_weight_reader(init_weight);
-    RandomAccessBaseFloatVectorReader init_bias_reader(init_bias);
-
-    BaseFloatMatrixWriter weight_writer(target_weight_wspecifier);
-    BaseFloatVectorWriter bias_writer(target_bias_wspecifier);
+    BaseFloatMatrixWriter weight_writer(weight_wspecifier);
+    BaseFloatVectorWriter bias_writer(bias_wspecifier);
 
     Xent xent;
 
-    CuMatrix<BaseFloat> feats, feats_transf, nnet_out, glob_err;
+    CuMatrix<BaseFloat> feats, feats_transf, nnet_out, glob_err, prev_weight;
+    CuVector<BaseFloat> prev_bias;
 
     Matrix<BaseFloat> lin_weight;
     Vector<BaseFloat> lin_bias;
 
-    Timer tim;
-    double time_next = 0;
-    KALDI_LOG<< (crossvalidate?"CROSSVALIDATE":"TRAINING") << " STARTED";
-
-    int32 num_done = 0, num_no_alignment = 0, num_other_error = 0,
-        num_use_init = 0, num_use_iden = 0;
-    for (; !feature_reader.Done(); ) {
+    int32 num_done = 0, num_no_alignment = 0, num_other_error = 0;
+    for (; !feature_reader.Done();) {
       std::string key = feature_reader.Key();
 
       if (!alignments_reader.HasKey(key)) {
         num_no_alignment++;
       } else {
+        Timer lin_timer;
+        /*
+         * LIN Training for the current utterance
+         */
+        KALDI_LOG << "============================================================";
+        KALDI_LOG << "*** Utterance: " << key;
+        KALDI_LOG << "Training started ...";
 
+        /* initialize variables */
+        bool havling = false;
+        BaseFloat acc, acc_new, acc_prev;
+        BaseFloat learn_rate = learn_rate_init_val;
+
+        /* Feature preparation */
         const Matrix<BaseFloat> &mat = feature_reader.Value();
         const std::vector<int32> &alignment = alignments_reader.Value(key);
 
@@ -128,66 +139,90 @@ int main(int argc, char *argv[]) {
           continue;
         }
 
-        if (num_done % 10000 == 0)
-          std::cout << num_done << ", " << std::flush;
-        num_done++;
+        KALDI_LOG << "[" << mat.NumRows() << " frames]";
 
-        // configure LIN
-        if (!crossvalidate) {
-          if (init_weight != "" && init_bias != ""
-              && init_weight_reader.HasKey(key)
-              && init_bias_reader.HasKey(key)) {
-            const Matrix<BaseFloat> init_wt = init_weight_reader.Value(key);
-            const Vector<BaseFloat> init_bs = init_bias_reader.Value(key);
+          /* forward feats through input transform if has */
+        feats.CopyFromMat(mat);  // push features to GPU
+        nnet_transf.Feedforward(feats, &feats_transf);
 
-            lin->SetLinearityWeight(init_wt, kNoTrans);
-            lin->SetBiasWeight(init_bs);
-            num_use_init++;
+        /* initialize si nnet */
+        lin->SetToIdentity();
+        nnet.SetLearnRate(learn_rate, learn_factors.c_str());
 
-          } else {
-            lin->SetToIdentity();
-            num_use_iden++;
+        /* pre-run cross-validation */
+        nnet.Propagate(feats_transf, &nnet_out);
+        xent.Reset();
+        xent.EvalVec(nnet_out, alignment, &glob_err);
+        acc=xent.GetFrameAccuracy();
+        KALDI_LOG << "CROSSVAL PRERUN ACCURACY " << acc ;
+
+        for(int32 iter=0; iter < max_iters; ++iter){
+          // keep a copy of the current model
+          prev_weight.CopyFromMat(lin->GetLinearityWeight());
+          prev_bias.CopyFromVec(lin->GetBiasWeight());
+
+          // update the model
+          nnet.Backpropagate(glob_err, NULL);
+
+          // cross-validation
+          nnet.Propagate(feats_transf, &nnet_out);
+          xent.Reset();
+          xent.EvalVec(nnet_out, alignment, &glob_err);
+          acc_new=xent.GetFrameAccuracy();
+
+          acc_prev = acc;
+          if(acc_new > acc){
+            // accept the weight
+            acc = acc_new;
+
+            KALDI_LOG << "ITERATION " << iter << ": LRATE " << learn_rate
+                << " CROSSVAL ACCURACY " << acc_new << " [accepted]";
+          }else{
+            // reject and revert back the weight
+            lin->SetLinearityWeight(prev_weight, false);
+            lin->SetBiasWeight(prev_bias);
+
+            KALDI_LOG << "ITERATION " << iter << ": LRATE " << learn_rate
+                << " CROSSVAL ACCURACY " << acc_new << " [rejected]";
+          }
+
+          if (learn_rate < learn_rate_end_val){
+            KALDI_LOG << "Too small learning rate " << learn_rate << "!";
+            break;
+          }
+
+          if (acc < acc_prev + start_halving_inc){
+            learn_rate = learn_rate * halving_factor;
+            nnet.SetLearnRate(learn_rate, learn_factors.c_str());
           }
         }
 
-        // push features to GPU
-        feats.CopyFromMat(mat);
+        /* save the LIN */
+        (lin->GetLinearityWeight()).CopyToMat(&lin_weight);
+        (lin->GetBiasWeight()).CopyToVec(&lin_bias);
 
-        nnet_transf.Feedforward(feats, &feats_transf);
-        nnet.Propagate(feats_transf, &nnet_out);
+        weight_writer.Write(key, lin_weight);
+        bias_writer.Write(key, lin_bias);
 
-        xent.EvalVec(nnet_out, alignment, &glob_err);
+        KALDI_LOG << "Training finished in " << lin_timer.Elapsed() << "s.";
+        KALDI_LOG << "============================================================";
 
-        if (!crossvalidate) {
-          nnet.Backpropagate(glob_err, NULL);
 
-          (lin->GetLinearityWeight()).CopyToMat(&lin_weight);
-          (lin->GetBiasWeight()).CopyToVec(&lin_bias);
-
-          weight_writer.Write(key, lin_weight);
-          bias_writer.Write(key, lin_bias);
+        ++num_done;
+        if(num_done % 1000 == 0){
+          KALDI_LOG << "[" << num_done << " done]";
         }
 
-        tot_t += mat.NumRows();
       }
 
-      Timer t_features;
       feature_reader.Next();
-      time_next += t_features.Elapsed();
     }
 
     std::cout << "\n" << std::flush;
 
-    KALDI_LOG<< (crossvalidate?"CROSSVALIDATE":"TRAINING") << " FINISHED "
-    << tim.Elapsed() << "s, fps" << tot_t/tim.Elapsed()
-    << ", feature wait " << time_next << "s";
-
     KALDI_LOG<< "Done " << num_done << " files, " << num_no_alignment
     << " with no alignments, " << num_other_error
     << " with other errors.";
-    KALDI_LOG<< num_use_init << " used initial LIN, " << num_use_iden << " used identity.";
-
-    KALDI_LOG<< xent.Report();
 
 #if HAVE_CUDA==1
     CuDevice::Instantiate().PrintProfile();
