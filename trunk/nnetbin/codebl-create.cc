81d033e5cdbf13160d3df129356ba0e4cd42e994
troy.lee2008@gmail.com <troy.lee2008@gmail.com@d2dc0ceb-8a10-0df8-ca54-0c7c990c9a2b>
2013-10-07 08:13:02 +0000

    revise codebl initilization

--- trunk/nnetbin/codebl-create.cc
+++ trunk/nnetbin/codebl-create.cc
@@ -20,8 +20,8 @@ int main(int argc, char *argv[]) {
     typedef kaldi::int32 int32;
 
     const char *usage =
-        "Creat <codebl> from a <biasedlinearity> layer\n"
-            "Usage: codebl-create [options] <bl-in> <codebl-out>"
+        "Convert all <biasedlinearity> layers to <codebl> \n"
+            "Usage: codebl-create [options] <bl-nnet-in> <codebl-nnet-out>"
             "e.g.: \n"
             " codebl-create --binary=false bl.nnet codebl.nnet \n";
 
@@ -52,38 +52,45 @@ int main(int argc, char *argv[]) {
       KALDI_LOG<< "Load model from " << bl_filename;
     }
 
-    KALDI_ASSERT(nnet.LayerCount()==1);
-    KALDI_ASSERT((nnet.Layer(0))->GetType() == Component::kBiasedLinearity);
-
-    BiasedLinearity *bl = static_cast<BiasedLinearity*>(nnet.Layer(0));
-    int32 bl_in_dim = bl->InputDim();
-    int32 bl_out_dim = bl->OutputDim();
-
-    Matrix<BaseFloat> linearity(bl_out_dim, bl_in_dim + code_dim, kSetZero),
-        bl_linearity;
-    Vector<BaseFloat> bias(bl_out_dim);
-
-    (bl->GetLinearityWeight()).CopyToMat(&bl_linearity);
-    (bl->GetBiasWeight()).CopyToVec(&bias);
-
-    (SubMatrix<BaseFloat>(linearity, 0, bl_out_dim, code_dim, bl_in_dim))
-        .CopyFromMat(bl_linearity);
-
-    // write the output
+      // write the output
     {
       Output ko(codebl_filename, binary);
 
-      WriteToken(ko.Stream(), binary,
-                 Component::TypeToMarker(Component::kCodeBL));
-      WriteBasicType(ko.Stream(), binary, bl_out_dim);
-      WriteBasicType(ko.Stream(), binary, bl_in_dim);
-      if (!binary)
-        ko.Stream() << "\n";
-      WriteBasicType(ko.Stream(), binary, code_dim);
-      if (!binary)
-        ko.Stream() << "\n";
-      linearity.Write(ko.Stream(), binary);
-      bias.Write(ko.Stream(), binary);
+      for (int32 i = 0; i < nnet.LayerCount(); ++i) {
+        if (nnet.Layer(i)->GetType() == Component::kBiasedLinearity) {
+
+          BiasedLinearity *bl = static_cast<BiasedLinearity*>(nnet.Layer(i));
+          int32 bl_in_dim = bl->InputDim();
+          int32 bl_out_dim = bl->OutputDim();
+
+          Matrix<BaseFloat> linearity(bl_out_dim, bl_in_dim + code_dim,
+                                      kSetZero),
+              bl_linearity;
+          Vector<BaseFloat> bias(bl_out_dim);
+
+          (bl->GetLinearityWeight()).CopyToMat(&bl_linearity);
+          (bl->GetBiasWeight()).CopyToVec(&bias);
+
+          (SubMatrix<BaseFloat>(linearity, 0, bl_out_dim, code_dim, bl_in_dim))
+              .CopyFromMat(bl_linearity);
+
+          WriteToken(ko.Stream(), binary,
+                     Component::TypeToMarker(Component::kCodeBL));
+          WriteBasicType(ko.Stream(), binary, bl_out_dim);
+          WriteBasicType(ko.Stream(), binary, bl_in_dim);
+          if (!binary)
+            ko.Stream() << "\n";
+          WriteBasicType(ko.Stream(), binary, code_dim);
+          if (!binary)
+            ko.Stream() << "\n";
+          linearity.Write(ko.Stream(), binary);
+          bias.Write(ko.Stream(), binary);
+
+        } else {
+          nnet.Layer(i)->Write(ko.Stream(), binary);
+        }
+
+      }
 
       KALDI_LOG<< "Write model to " << codebl_filename;
     }
