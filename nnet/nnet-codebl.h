135b63e8094ae92ba43cd0a891e43881f2f3dbbf
troy.lee2008@gmail.com <troy.lee2008@gmail.com@d2dc0ceb-8a10-0df8-ca54-0c7c990c9a2b>
2013-10-07 11:40:04 +0000

    enable learning only the codevec

--- nnet/nnet-codebl.h
+++ nnet/nnet-codebl.h
@@ -23,13 +23,15 @@ namespace kaldi {
 class CodeBL : public BiasedLinearity {
  public:
   CodeBL(MatrixIndexT dim_in, MatrixIndexT dim_out, Nnet *nnet)
- : BiasedLinearity(dim_in, dim_out, nnet),
-   code_dim_(0)
- {
+      : BiasedLinearity(dim_in, dim_out, nnet),
+        code_dim_(0),
+        update_weight_(true)
+  {
 
- }
+  }
 
-  ~CodeBL(){}
+  ~CodeBL() {
+  }
 
   ComponentType GetType() const {
     return kCodeBL;
@@ -67,13 +69,14 @@ class CodeBL : public BiasedLinearity {
 
   void PropagateFnc(const CuMatrix<BaseFloat> &in, CuMatrix<BaseFloat> *out) {
     // augment the input if needed
-    if (code_dim_ > 0){
-      if(augmented_in_.NumRows()!=in.NumRows() || augmented_in_.NumCols()!=in.NumCols() + code_dim_){
+    if (code_dim_ > 0) {
+      if (augmented_in_.NumRows() != in.NumRows()
+          || augmented_in_.NumCols() != in.NumCols() + code_dim_) {
         augmented_in_.Resize(in.NumRows(), in.NumCols() + code_dim_);
       }
       augmented_in_.AddVecToPartialRows(1.0, 0, code_vec_, 0.0);
       augmented_in_.PartAddMat(1.0, in, 0, code_dim_, 0.0);
-    }else{
+    } else {
       augmented_in_.CopyFromMat(in);
     }
 
@@ -83,45 +86,58 @@ class CodeBL : public BiasedLinearity {
     out->AddMatMat(1.0, augmented_in_, kNoTrans, linearity_, kTrans, 1.0);
   }
 
-  void BackpropagateFnc(const CuMatrix<BaseFloat> &in_err, CuMatrix<BaseFloat> *out_err) {
+  void BackpropagateFnc(const CuMatrix<BaseFloat> &in_err,
+                        CuMatrix<BaseFloat> *out_err) {
     // multiply error by weights
-    if(augmented_err_.NumRows()!=in_err.NumRows() || augmented_err_.NumCols() != linearity_.NumCols()){
+    if (augmented_err_.NumRows() != in_err.NumRows()
+        || augmented_err_.NumCols() != linearity_.NumCols()) {
       augmented_err_.Resize(in_err.NumRows(), linearity_.NumCols());
     }
     augmented_err_.AddMatMat(1.0, in_err, kNoTrans, linearity_, kNoTrans, 0.0);
 
-    if(code_dim_>0){
-      code_err_.CopyFromMat(augmented_err_, 0, augmented_err_.NumRows(), 0, code_dim_);
-      out_err->CopyFromMat(augmented_err_, 0, augmented_err_.NumRows(), code_dim_, augmented_err_.NumCols()-code_dim_);
-    }else{
+    if (code_dim_ > 0) {
+      code_err_.CopyFromMat(augmented_err_, 0, augmented_err_.NumRows(), 0,
+                            code_dim_);
+      out_err->CopyFromMat(augmented_err_, 0, augmented_err_.NumRows(),
+                           code_dim_, augmented_err_.NumCols() - code_dim_);
+    } else {
       out_err->CopyFromMat(augmented_err_);
     }
   }
 
-  void Update(const CuMatrix<BaseFloat> &input, const CuMatrix<BaseFloat> &err) {
+  void Update(const CuMatrix<BaseFloat> &input,
+              const CuMatrix<BaseFloat> &err) {
     // compute gradient
-    if(average_grad_){
-      linearity_corr_.AddMatMat(1.0 / input.NumRows(), err, kTrans, augmented_in_, kNoTrans, momentum_);
-      bias_corr_.AddRowSumMat(1.0 / input.NumRows(), err, momentum_);
+    if (average_grad_) {
+      if (update_weight_) {
+        linearity_corr_.AddMatMat(1.0 / input.NumRows(), err, kTrans,
+                                  augmented_in_, kNoTrans, momentum_);
+        bias_corr_.AddRowSumMat(1.0 / input.NumRows(), err, momentum_);
+      }
       code_corr_.AddRowSumMat(1.0 / input.NumRows(), code_err_, momentum_);
-    }else {
-      linearity_corr_.AddMatMat(1.0, err, kTrans, augmented_in_, kNoTrans, momentum_);
-      bias_corr_.AddRowSumMat(1.0, err, momentum_);
+    } else {
+      if (update_weight_) {
+        linearity_corr_.AddMatMat(1.0, err, kTrans, augmented_in_, kNoTrans,
+                                  momentum_);
+        bias_corr_.AddRowSumMat(1.0, err, momentum_);
+      }
       code_corr_.AddRowSumMat(1.0, code_err_, momentum_);
     }
     // l2 regularization
-    if (l2_penalty_ != 0.0){
+    if (l2_penalty_ != 0.0 && update_weight_) {
       BaseFloat l2 = learn_rate_ * l2_penalty_ * input.NumRows();
       linearity_.AddMat(-l2, linearity_);
     }
     // l1 regularization
-    if (l1_penalty_ != 0.0){
+    if (l1_penalty_ != 0.0 && update_weight_) {
       BaseFloat l1 = learn_rate_ * input.NumRows() * l1_penalty_;
       cu::RegularizeL1(&linearity_, &linearity_corr_, l1, learn_rate_);
     }
     // update
-    linearity_.AddMat(-learn_rate_, linearity_corr_);
-    bias_.AddVec(-learn_rate_, bias_corr_);
+    if (update_weight_) {
+      linearity_.AddMat(-learn_rate_, linearity_corr_);
+      bias_.AddVec(-learn_rate_, bias_corr_);
+    }
 
     // scale the code_corr_ with current layer's learn rate
     code_corr_.Scale(-learn_rate_);
@@ -132,11 +148,11 @@ class CodeBL : public BiasedLinearity {
    * The update of the code vector is done outside as it may be shared
    * among different layers.
    */
-  const CuVector<BaseFloat>& GetCodeVecCorr(){
+  const CuVector<BaseFloat>& GetCodeVecCorr() {
     return code_corr_;
   }
 
-  void SetCodeVec(const CuVector<BaseFloat> &vec){
+  void SetCodeVec(const CuVector<BaseFloat> &vec) {
     code_vec_.CopyFromVec(vec);
   }
 
@@ -151,20 +167,28 @@ class CodeBL : public BiasedLinearity {
     return code_dim_;
   }
 
+  /*
+   * Enable/Disable weight update.
+   */
+  void ConfigWeightUpdate(bool enable) {
+    update_weight_ = enable;
+  }
+
  protected:
 
-  int32 code_dim_; // dimensionality of the code
+  bool update_weight_;
 
-  CuMatrix<BaseFloat> augmented_in_; // input feature + code
-  CuVector<BaseFloat> code_vec_; // code vector
+  int32 code_dim_;  // dimensionality of the code
 
-  CuMatrix<BaseFloat> augmented_err_; // the error includes code errors
-  CuMatrix<BaseFloat> code_err_; // error matrix for code vector
+  CuMatrix<BaseFloat> augmented_in_;  // input feature + code
+  CuVector<BaseFloat> code_vec_;  // code vector
 
-  CuVector<BaseFloat> code_corr_; // correction of the code vector
+  CuMatrix<BaseFloat> augmented_err_;  // the error includes code errors
+  CuMatrix<BaseFloat> code_err_;  // error matrix for code vector
+
+  CuVector<BaseFloat> code_corr_;  // correction of the code vector
 };
 
-} // namespace
-
+}  // namespace
 
 #endif /* NNET_CODEBL_H_ */
