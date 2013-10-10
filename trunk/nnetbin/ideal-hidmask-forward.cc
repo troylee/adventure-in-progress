ede64d424028b257b11d0efcd72bd0c3a068af7e
troy.lee2008@gmail.com <troy.lee2008@gmail.com@d2dc0ceb-8a10-0df8-ca54-0c7c990c9a2b>
2013-10-10 03:55:19 +0000

    bug fix the compuation of mask

--- trunk/nnetbin/ideal-hidmask-forward.cc
+++ trunk/nnetbin/ideal-hidmask-forward.cc
@@ -152,6 +152,7 @@ int main(int argc, char *argv[]) {
       hidmask.CopyFromMat(l1_out);
       hidmask.AddMat(-1.0, ref_l1_out, 1.0);
       hidmask.Power(2.0);
+      hidmask.Scale(-1.0);
       hidmask.ApplyExp();
 
       l1_out.MulElements(hidmask);
